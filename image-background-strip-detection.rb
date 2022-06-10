a22ae668f402a6d04ba09a5e900a899b7c76cbcb
Coro365 <coro.365@gmail.com>
2022-06-11 01:41:03 +0900

    add horizon_strip_detect method

--- image-background-strip-detection.rb
+++ image-background-strip-detection.rb
@@ -5,8 +5,8 @@ module Imagemagick
     open(cmd, 'r')
   end
 
-  def to_color_code
-    convert(self).extract_color_code
+  def to_color_code(crop=nil)
+    convert(self, crop).extract_color_code
   end
 
   def extract_color_code
@@ -29,6 +29,49 @@ def full_scan(image_file)
   pp image_file.to_color_code
 end
 
-image_file = './test/67417700_p0_30p.jpg'
+def horizon_strip_detect(image_file)
+  img_geo = image_file.image_geometry
+  scan_bandwidth_num = (img_geo[:height] * SCAN_BANDWIDTH_PERCENT * 0.01).to_i
 
-full_scan(image_file)
+  range_top = (1..scan_bandwidth_num)
+  top = borderline(image_file, range_top)
+
+  range_bottom = (img_geo[:height]-1..img_geo[:height]-scan_bandwidth_num)
+  bottom = borderline(image_file, range_bottom)
+
+  {top: top + OFFSET_LINE_NUM, bottom: bottom - OFFSET_LINE_NUM}
+end
+
+def borderline(image_file, range)
+  # TODO: refactoring
+  if range.first < range.last
+    range.each do |line_num|
+      result = color_num_threshold_cheack(image_file, line_num)
+      break result if result
+    end
+  else
+    (range.first).downto(range.last) do |line_num|
+      result = color_num_threshold_cheack(image_file, line_num)
+      break result if result
+    end
+  end
+end
+
+def color_num_threshold_cheack(image_file, line_num)
+  color_num = line_colors(image_file, line_num).size
+  COLOR_NUM_THRESHOLD < color_num ? line_num : false
+end
+
+def line_colors(image_file, line_num)
+  crop = {width: 0, height: 1, geo_x: 0, geo_y: line_num}
+  colors = image_file.to_color_code(crop)
+
+  colors.sort.uniq.map do |color|
+    {code: color, count: colors.count(color)}
+  end.sort_by{ |h| h.values_at(:count) }.reverse
+end
+
+
+COLOR_NUM_THRESHOLD = 50
+OFFSET_LINE_NUM = 2
+SCAN_BANDWIDTH_PERCENT = 30
